3df3edcc43822d901f860459f1d2c8e458009646
ChrisGn <christoph.gnoth@gmx.de>
2020-05-24 20:35:34 +0200

    Delete Main_TestAll.cpp

--- test/Main_TestAll.cpp
+++
@@ -1,9 +0,0 @@
-#include "gtest/gtest.h"
-
-
-//source: http://www.yolinux.com/TUTORIALS/Cpp-GoogleTest.html
-
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
